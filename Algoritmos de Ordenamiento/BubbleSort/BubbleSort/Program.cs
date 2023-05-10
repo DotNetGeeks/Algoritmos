ff96dfcdce9d38f66f37d32efa77560042e6839a
dotnetgeeks <dotnetgeeks1718@gmail.com>
2023-05-09 21:33:42 -0600

    Insert Sort

---
+++ Algoritmos de Ordenamiento/BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace BubbleSort
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int[] data = new int[] { 1, 20, 400, 605, -5, 0, 100, 100000, 77 };
+
+            Console.WriteLine("----- Datos sin ordenar ------");
+            printArray(data);
+
+            Console.WriteLine("----- Datos ordenados ------");
+            bubbleSort(data);
+            printArray(data);
+        }
+
+        static void bubbleSort(int[] arr)
+        {
+            int arrSize = arr.Length;
+            for (int i = 0; i < arrSize; i++)
+            {
+                for (int j = 0; j < arrSize - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int tempo = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = tempo;
+                    }
+                }
+            }
+        }
+
+        static void printArray(int[] arr)
+        {
+            int arrSize = arr.Length;
+            for (int i = 0; i < arrSize; i++)
+            {
+                Console.WriteLine($"Posicion: {i} | Valor: {arr[i]}");
+            }
+        }
+    }
+}
---
+++ Algoritmos de Ordenamiento/InsertionSort/InsertionSort/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace InsertionSort
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] data = new int[] { 1, 20, 400, 605, -5, 0, 100, 100000, 77 };
+
+            Console.WriteLine("----- Datos sin ordenar ------");
+            printArray(data);
+
+            Console.WriteLine("----- Datos ordenados ------");
+            InsertionSort(data);
+            printArray(data);
+        }
+
+        static void InsertionSort(int[] data)
+        {
+            for (int i = 1; i < data.Length; i++)
+            {
+                int key = data[i];
+                int j = i - 1;
+                while (j >= 0 && data[j] > key) 
+                {
+                    data[j + 1] = data[j];
+                    j = j - 1;
+                }
+
+                data[j + 1] = key;
+            }
+        }
+
+
+        static void printArray(int[] arr)
+        {
+            int arrSize = arr.Length;
+            for (int i = 0; i < arrSize; i++)
+            {
+                Console.WriteLine($"Posicion: {i} | Valor: {arr[i]}");
+            }
+        }
+    }
+}
